daba090748ff8c76186e2ae919d5994d1dcfbac5
Mads Lee Giil <mads.lee.giil@nav.no>
2023-12-27 12:59:11 +0100

    Make tests pass

--- src/main/kotlin/no/nav/please/plugins/TokenProvider.kt
+++ src/main/kotlin/no/nav/please/plugins/TokenProvider.kt
@@ -6,8 +6,15 @@ import no.nav.common.token_client.builder.AzureAdTokenClientBuilder
 typealias GetMachineToMachineToken = (scope: String) -> String
 
 fun Application.configureTokenProvider(): GetMachineToMachineToken {
+    val config = this.environment.config
+    val azureClientId = config.property("azure.client-id").getString()
+    val privateJwk = config.property("azure.jwk").getString()
+    val tokenEndpoint = config.property("azure.token-endpoint").getString()
+
     val tokenClient = AzureAdTokenClientBuilder.builder()
-        .withNaisDefaults()
+        .withClientId(azureClientId)
+        .withPrivateJwk(privateJwk)
+        .withTokenEndpointUrl(tokenEndpoint)
         .buildMachineToMachineTokenClient()
 
     return { scope ->
