7212d884f1995dc3274b92a817e62c5c4b6b882b
Mads Lee Giil <mads.lee.giil@nav.no>
2024-01-03 22:53:45 +0100

    WIP

--- src/main/kotlin/no/nav/please/plugins/HttpClient.kt
+++ src/main/kotlin/no/nav/please/plugins/HttpClient.kt
@@ -1,9 +1,23 @@
 package no.nav.please.plugins
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 
-
-fun Application.configureMachineToMachineHttpClient(getMachineToMachineToken: MachineToMachineToken, getRefreshedMachineToMachineToken: RefreshMachineToMachineToken): HttpClient {
-
+fun Application.machineToMachineClient(): HttpClient {
+    return HttpClient(OkHttp) {
+        engine {
+            config {
+                followRedirects(true)
+            }
+        }
+        install(ContentNegotiation) {
+            jackson {
+                this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            }
+        }
+    }
 }
