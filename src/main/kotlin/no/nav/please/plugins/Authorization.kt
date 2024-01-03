445a940f9d5b84e2bb7bd350f7a8a6458ce4e952
Mads Lee Giil <mads.lee.giil@nav.no>
2024-01-03 22:53:45 +0100

    WIP

--- src/main/kotlin/no/nav/please/plugins/Authorization.kt
+++ src/main/kotlin/no/nav/please/plugins/Authorization.kt
@@ -6,29 +6,25 @@ import java.util.*
 //
 //typealias navEmployeeIsAuthorized = (employeeAzureId: UUID, externalUserIdentityNumber: String) -> Boolean // TODO: Få typesatt på annet vis en typealias
 //
-//fun Application.configureAuthorization(getMachineToMachineToken: MachineToMachineToken, getRefreshedMachineToMachineToken: RefreshMachineToMachineToken): navEmployeeIsAuthorized {
+//fun Application.configureAuthorization(getToken: (scope: String) -> String): navEmployeeIsAuthorized {
+//    val config = this.environment.config
+//    val poaoTilgangUrl = config.property("poao-tilgang.url").getString()
+//    val tokenScope = config.property("poao-tilgang.token-scope").getString()
 //
+//    val client: PoaoTilgangClient = PoaoTilgangCachedClient(
+//        PoaoTilgangHttpClient(
+//            baseUrl = poaoTilgangUrl,
+//            tokenProvider = { getToken(tokenScope) }
+//        )
+//    )
 //
+//    return { employeeAzureId: UUID, externalUserIdentityNumber: String ->
+//        val decision = client.evaluatePolicy(NavAnsattTilgangTilEksternBrukerPolicyInput(
+//            navAnsattAzureId = employeeAzureId,
+//            tilgangType = TilgangType.SKRIVE, // TODO: Decide on what is the correct permission
+//            norskIdent = externalUserIdentityNumber
+//        )).get()
 //
-//
-////    val config = this.environment.config
-////    val poaoTilgangUrl = config.property("poao-tilgang.url").getString()
-////    val tokenScope = config.property("poao-tilgang.token-scope").getString()
-////
-////    val client: PoaoTilgangClient = PoaoTilgangCachedClient(
-////        PoaoTilgangHttpClient(
-////            baseUrl = poaoTilgangUrl,
-////            tokenProvider = { getMachineToMachineToken(tokenScope)}
-////        )
-////    )
-////
-////    return { employeeAzureId: UUID, externalUserIdentityNumber: String ->
-////        val decision = client.evaluatePolicy(NavAnsattTilgangTilEksternBrukerPolicyInput(
-////            navAnsattAzureId = employeeAzureId,
-////            tilgangType = TilgangType.SKRIVE, // TODO: Decide on what is the correct permission
-////            norskIdent = externalUserIdentityNumber
-////        )).get()
-////
-////        decision?.isPermit ?: false
-////    }
+//        decision?.isPermit ?: false
+//    }
 //}
