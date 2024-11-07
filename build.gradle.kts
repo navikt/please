d1185e9d34410edbe5b885abb3c398880b3697e4
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2024-11-07 13:22:52 +0100

    Update jib plugin for docker build

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ plugins {
     kotlin("jvm") version "1.9.10"
     id("io.ktor.plugin") version "2.3.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+    id("com.google.cloud.tools.jib") version "3.4.4"
 }
 
 group = "no.nav.please"
