7e081f814141644c1535a25c42418772ef0f0691
sigurdgroneng <sigurd.groneng@nav.no>
2025-10-09 08:37:26 +0200

    Try to fix gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ val arrow_version: String by project
 
 plugins {
     kotlin("jvm") version "2.2.20"
-    id("io.ktor.plugin") version "3.3.1"
+    id("io.ktor.plugin") version "3.3.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
 }
 
