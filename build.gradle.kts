426d8d8d108d43d4253cf2354d4408bcc41bd390
tiafiring <tia.firing@nav.no>
2026-05-08 13:30:27 +0200

    Oppdateringer (#19)
    * oppdaterer common, poao-tilgang og testavhengighet for ktor
    
    * oppdaterer avhengigheter som bør være uproblematiske
    
    * oppdaterer ktor og tokensupport, med forslag fra copilot for at det skal virke

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import io.ktor.plugin.features.*
 
 val ktor_version: String by project
-val ktor_server_tests_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val tokensupport_version: String by project
@@ -17,7 +16,7 @@ val common_version: String by project
 
 plugins {
     kotlin("jvm") version "2.2.20"
-    id("io.ktor.plugin") version "3.3.0"
+    id("io.ktor.plugin") version "3.4.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
 }
 
@@ -76,7 +75,7 @@ dependencies {
     implementation("io.ktor:ktor-server-call-id-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:1.11")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
 
@@ -85,7 +84,6 @@ dependencies {
     implementation("io.ktor:ktor-client-auth:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-client-logging:$ktor_version")
-//    implementation("io.ktor:ktor-serialization-jackson:$ktor_version")
 
     implementation("io.ktor:ktor-server-auth:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
@@ -95,7 +93,7 @@ dependencies {
     implementation("io.arrow-kt:arrow-core:$arrow_version")
     implementation("io.arrow-kt:arrow-fx-coroutines:$arrow_version")
     implementation("io.arrow-kt:arrow-resilience-jvm:$arrow_version")
-    implementation("org.slf4j:slf4j-api:1.7.25")
+    implementation("org.slf4j:slf4j-api:2.0.17")
 
     implementation("net.logstash.logback:logstash-logback-encoder:$logstash_encoder_version")
 
@@ -109,11 +107,11 @@ dependencies {
 
     testImplementation("io.kotest:kotest-runner-junit5:$kotest_version")
     testImplementation("io.kotest:kotest-assertions-core:$kotest_version")
-    testImplementation("io.kotest:kotest-extensions-wiremock:$kotest_extensions_version")
-    testImplementation("org.signal:embedded-redis:0.8.3")
+    testImplementation("io.kotest:kotest-extensions-wiremock-jvm:$kotest_extensions_version")
+    testImplementation("org.signal:embedded-redis:0.9.1")
 
     testImplementation("no.nav.security:mock-oauth2-server:$mockoath_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_server_tests_version")
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
     testImplementation("io.ktor:ktor-client-websockets-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
