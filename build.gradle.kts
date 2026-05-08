41f115b8cc8a45214d95ea2cda5a5a67467dec7b
Tia Firing <tia.firing@nav.no>
2026-05-08 09:53:55 +0200

    oppdaterer avhengigheter som bør være uproblematiske

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation("io.ktor:ktor-server-call-id-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:1.11")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
 
@@ -84,7 +84,6 @@ dependencies {
     implementation("io.ktor:ktor-client-auth:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-client-logging:$ktor_version")
-//    implementation("io.ktor:ktor-serialization-jackson:$ktor_version")
 
     implementation("io.ktor:ktor-server-auth:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
@@ -94,7 +93,7 @@ dependencies {
     implementation("io.arrow-kt:arrow-core:$arrow_version")
     implementation("io.arrow-kt:arrow-fx-coroutines:$arrow_version")
     implementation("io.arrow-kt:arrow-resilience-jvm:$arrow_version")
-    implementation("org.slf4j:slf4j-api:1.7.25")
+    implementation("org.slf4j:slf4j-api:2.0.17")
 
     implementation("net.logstash.logback:logstash-logback-encoder:$logstash_encoder_version")
 
@@ -108,8 +107,8 @@ dependencies {
 
     testImplementation("io.kotest:kotest-runner-junit5:$kotest_version")
     testImplementation("io.kotest:kotest-assertions-core:$kotest_version")
-    testImplementation("io.kotest:kotest-extensions-wiremock:$kotest_extensions_version")
-    testImplementation("org.signal:embedded-redis:0.8.3")
+    testImplementation("io.kotest:kotest-extensions-wiremock-jvm:$kotest_extensions_version")
+    testImplementation("org.signal:embedded-redis:0.9.1")
 
     testImplementation("no.nav.security:mock-oauth2-server:$mockoath_version")
     testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
