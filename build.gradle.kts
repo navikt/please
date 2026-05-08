c7f694b92e19de87dcff1143304f3f6b17f0652b
Tia Firing <tia.firing@nav.no>
2026-05-08 10:28:44 +0200

    oppdaterer ktor og tokensupport, med forslag fra copilot for at det skal virke

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val common_version: String by project
 
 plugins {
     kotlin("jvm") version "2.2.20"
-    id("io.ktor.plugin") version "3.3.0"
+    id("io.ktor.plugin") version "3.4.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
 }
 
